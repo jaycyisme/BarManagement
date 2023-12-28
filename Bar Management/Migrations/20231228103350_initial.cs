4d76f0c62a010943bb23b7afadde5db52de9a2e6
Dai Dong <phungdaidong1114@gmail.com>
2023-12-28 17:40:48 +0700

    Nhap Nhat appdbcontext

---
+++ Bar Management/Migrations/20231228103350_initial.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Bar_Management.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TrangThaiTonKho",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Ten = table.Column<string>(maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TrangThaiTonKho", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TrangThaiTonKho");
+        }
+    }
+}
---
+++ Bar Management/Models/TrangThaiTonKho.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bar_Management.Models {
+    internal class TrangThaiTonKho {
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(50), DataType("nvarchar")]
+        public string Ten { get; set; }
+    }
+}
